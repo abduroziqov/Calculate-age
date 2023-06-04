138ac43d888a005d50cfabc279414932f80c63cb
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2023-06-04 17:04:16 +0500

    The program calculates person's age

--- CalculateAge/CalculateAge/Program.cs
+++ CalculateAge/CalculateAge/Program.cs
@@ -6,15 +6,18 @@
         {
             CalculateAge();
             Main();
+
         }
         public static void CalculateAge()
         {
+        
             DateTime dateTime = DateTime.Now;
 
             DateOnly dateOnly = DateOnly.FromDateTime(dateTime);
 
             try
             {
+               
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("------Welcome to age calculate program------");
 
@@ -28,17 +31,20 @@
                 int birthDay = int.Parse(Console.ReadLine());
 
                 int calculateYear = dateOnly.Year - birthYear;
-                int calculateMonth = dateOnly.Month - birthMonth;
+              
 
-                if (birthDay < dateOnly.Day)
+                if (birthDay < dateOnly.Day || birthMonth < dateOnly.Month )
                 {
                     int calculateDay = (dateOnly.Day) - birthDay;
+                    int calculateMonth = dateOnly.Month - birthMonth;
 
                     Console.WriteLine($"The person was born {calculateMonth} months {calculateDay} days {calculateYear} years ago.\n");
                 }
                 else
                 {
+                    calculateYear = calculateYear - 1;
                     int calculateDay = (dateOnly.Day + 30) - birthDay;
+                    int calculateMonth = (dateOnly.Month + 12) - birthMonth;
                     Console.WriteLine($"The person was born {calculateMonth} months {calculateDay} days {calculateYear} years ago.\n");
                 }
             }
