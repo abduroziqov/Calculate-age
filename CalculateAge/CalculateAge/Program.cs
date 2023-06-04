58d15e0e33e49ce1abeb0e96e9e1a5093a99d1f3
Sobirjon Matnazarov <matnazarovsobirjon123@outlook.com>
2023-06-04 16:40:53 +0500

    #closes_main Answer metodi qo'shdim, va console orqali kelishi mumkin bo'lgan null oldini oldim.

--- CalculateAge/CalculateAge/Program.cs
+++ CalculateAge/CalculateAge/Program.cs
@@ -1,11 +1,45 @@
-﻿namespace CalculateAge
+﻿using System.Runtime.CompilerServices;
+
+namespace CalculateAge
 {
     internal class Program
     {
         static void Main()
         {
-            CalculateAge();
-            Main();
+            bool progress = true;
+            
+            while (progress)
+            {
+                CalculateAge();
+                progress = Answer();
+            }
+        }
+
+        public static bool Answer()
+        {
+            bool result = true;
+            
+            Console.WriteLine("Do you have a continue? (yes, no)");
+            
+            while(result)
+            {
+                string? answer = Console.ReadLine();
+                if(answer == "no")
+                {
+                    Console.Clear();
+                    result = false;
+                }
+                else if (answer == "yes")
+                {
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect syntax, try again...");
+                }
+            }
+            return result;
         }
         public static void CalculateAge()
         {
@@ -18,16 +52,20 @@
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("------Welcome to age calculate program------");
 
+                #region Receive Values From User
                 Console.Write("Enter year of birth : ");
-                int birthYear = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out int birthYear);
 
                 Console.Write("Enter month of birth : ");
-                int birthMonth = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out int birthMonth);
 
                 Console.Write("Enter day of birth : ");
-                int birthDay = int.Parse(Console.ReadLine());
+                int.TryParse(Console.ReadLine(), out int birthDay);
+                #endregion
 
+                #region Calculates Values To Human Readable Format
                 int calculateYear = dateOnly.Year - birthYear;
+
                 int calculateMonth = dateOnly.Month - birthMonth;
 
                 if (birthDay < dateOnly.Day)
@@ -41,15 +79,14 @@
                     int calculateDay = (dateOnly.Day + 30) - birthDay;
                     Console.WriteLine($"The person was born {calculateMonth} months {calculateDay} days {calculateYear} years ago.\n");
                 }
+                #endregion
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 Console.Clear();
-                Console.WriteLine("Please enter valid value.");
+                Console.WriteLine("Please enter valid value...");
                 CalculateAge();
             }
-            Console.ReadKey();
-            Console.Clear();
         }
     }
 }
