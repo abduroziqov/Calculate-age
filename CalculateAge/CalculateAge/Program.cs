c6b62c3a524990c3d0054a78dacebe414e8a21b8
Sobirjon Matnazarov <matnazarovsobirjon123@outlook.com>
2023-06-04 21:19:35 +0500

    over fix

--- CalculateAge/CalculateAge/Program.cs
+++ CalculateAge/CalculateAge/Program.cs
@@ -1,90 +1,34 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-
-namespace CalculateAge
+﻿namespace CalculateAge
 {
     internal class Program
     {
+        static void CalcDifference(string Date)
+        {
+            DateTime nowDate = DateTime.Now;
+            string[] lastDate = Date.Split('/');
+
+            int Month = nowDate.Month - Convert.ToInt32(lastDate[1]);
+
+            int Day = DateTime.DaysInMonth(Convert.ToInt32(lastDate[2]), Convert.ToInt32(lastDate[1])) - Convert.ToInt32(lastDate[0]);
+
+            int Year = nowDate.Year;
+
+            if (nowDate.Month >= Convert.ToInt32(lastDate[1]))
+            {
+                Year -= Convert.ToInt32(lastDate[2]);
+            }
+            else
+            {
+                Year -= Convert.ToInt32(lastDate[2]) + 1;
+            }
+            
+
+            Console.WriteLine($"Person was born {Day} days {Month} months {Year} years ago.");
+        }
+        
         static void Main()
         {
-            bool progress = true;
-            
-            while (progress)
-            {
-                CalculateAge();
-                progress = Answer();
-            }
-        }
-
-        public static bool Answer()
-        {
-            bool result = true;
-            
-            Console.WriteLine("Do you have a continue? (yes, no)");
-            
-            while(result)
-            {
-                string? answer = Console.ReadLine();
-                if(answer == "no")
-                {
-                    Console.Clear();
-                    result = false;
-                }
-                else if (answer == "yes")
-                {
-                    Console.Clear();
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect syntax, try again...");
-                }
-            }
-            return result;
-        }
-        public static void CalculateAge()
-        {
-            DateOnly nowDate = DateOnly.FromDateTime(DateTime.Now);
-
-            try
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("------Welcome to age calculate program------");
-
-                #region Receive Values From User
-                Console.Write("Enter year of birth : ");
-                int birthYear = int.Parse(Console.ReadLine());
-
-                Console.Write("Enter month of birth : ");
-                int birthMonth = int.Parse(Console.ReadLine());
-
-                Console.Write("Enter day of birth : ");
-                int birthDay = int.Parse(Console.ReadLine());
-                #endregion
-
-                #region Calculates Values To Human Readable Format
-                DateOnly.TryParse($"{birthMonth}/{birthDay}/{birthYear}", out DateOnly lastDate);
-
-                int calculateYear = nowDate.Year - lastDate.Year;
-                
-                int calculateMonth = (nowDate.Month < lastDate.Month) ? 
-                                     (nowDate.Month + 12) - lastDate.Month : 
-                                     calculateMonth = nowDate.Month - lastDate.Month;
-                
-
-                int calculateDay = (nowDate.Day < lastDate.Day) ? 
-                                   DateTime.DaysInMonth(nowDate.Year, nowDate.Month) - lastDate.Day :
-                                   nowDate.Day - lastDate.Day;
-
-                Console.WriteLine($"The person was born {calculateMonth} months {calculateDay} days {calculateYear} years ago.\n");
-                #endregion
-            }
-            catch (Exception)
-            {
-                Console.Clear();
-                Console.WriteLine("Please enter valid value...");
-                CalculateAge();
-            }
+            CalcDifference("12/04/2003");
         }
     }
 }
